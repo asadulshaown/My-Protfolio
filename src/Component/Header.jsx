b28bf70081059fcbc564ff2423d64c7c360372a3
asa <asa27403@gmail.com>
2025-06-01 23:54:00 +0600

    add images

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
   return (
     <header className=' container mt-1 mb-4 grid grid-cols-3 sticky top-0  bg-slate-100 overflow-x-hidden shadow-md shadow-slate-500 z-10'>
-      <img  className='w-32 h-32 border-8 m-1 border-black  rounded-full ' src="/public/Images/profile.jpg" alt="Profile Picture" />
+      <img  className='w-32 h-32 border-8 m-1 border-black  rounded-full ' src="/Images/profile.jpg" alt="Profile Picture" />
        <div className=''>
        <h1 className='logo mt-2 text-5xl md:text-6xl lg:text-7xl   font-serif font-bold'>
       AS ASA      
